23d6d79ee6290e6189dc81516757433825dc191e
SoldatovaTania <tatyana.soldatova.96@mail.ru>
2022-12-06 23:10:11 +0300

    Домашка
    Добавила домашку

---
+++ Program 3.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+using System;
+using System.Globalization;
+ 
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+                int Value = Convert.ToInt32(Console.Read());
+                if (Value % 2 == 0) Console.WriteLine("Четное");
+                else Console.WriteLine("Нечетное");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+using System;
+
+namespace Sem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Value_1 = Console.ReadLine();
+            int parsedValue1 = int.Parse(Value_1);
+
+            string Value_2 = Console.ReadLine();
+            int parsedValue2 = int.Parse(Value_2);
+
+            if (parsedValue1 > parsedValue2)
+            {
+                Console.WriteLine ("Первое число больше второго");
+            }
+            else 
+            {
+                 Console.WriteLine ("Второе число больше первого"); 
+            }
+           
+        }
+    }
+}
